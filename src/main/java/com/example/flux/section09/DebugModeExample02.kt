0050202bc43aa2c3d49ec6dfc1e27928d9d43a0b
leesoobeen <02ggang9@gmail.com>
2024-03-29 23:57:14 +0900

    feat: Hooks.onOperator 사용

---
+++ src/main/java/com/example/flux/section09/DebugModeExample02.kt
@@ -0,0 +1,12 @@
+package com.example.flux.section09
+
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Hooks
+
+fun main() {
+    Hooks.onOperatorDebug()
+
+    Flux.just(2,4,6,8)
+        .zipWith(Flux.just(1,2,3,0)) { a, b -> a / b }
+        .subscribe { println("onNext: $it") }
+}
---
+++ src/main/java/com/example/flux/section09/DebugModeExample03.kt
@@ -0,0 +1,23 @@
+package com.example.flux.section09
+
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Hooks
+
+fun main() {
+    Hooks.onOperatorDebug()
+    val fruits = mapOf(
+        "banana" to "바나나",
+        "apple" to "사과",
+        "pear" to "배",
+        "grape" to "포도"
+    )
+
+    Flux.fromArray(arrayOf("BANANAS", "APPLES", "PEARS", "MELONS"))
+        .map { it.lowercase() }
+        .map { it.substring(0, it.length-1) }
+        .map { fruits[it] }
+        .map { "맛있는 $it" }
+        .subscribe { println("onNext: $it") }
+
+
+}
